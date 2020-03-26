85823643cca60f5a54114ad1b758ebcdb04e253c
raysinensis <raysinensis@gmail.com>
2020-03-25 19:55:35 -0600

    updated script

--- inst/run_clustifyr.R
+++ inst/run_clustifyr.R
@@ -4,7 +4,9 @@ run_clustifyr <- function(DataPath,
                           OutputDir,
                           GeneOrderPath = NULL,
                           NumGenes = NULL,
-                          threshold = 0.75) {
+                          threshold = 0.75,
+                          compute_method = "spearman"
+                          ) {
   "
   run clustifyr
   Wrapper script to run clustifyr on a benchmark dataset with 5-fold cross validation,
@@ -24,6 +26,8 @@ run_clustifyr <- function(DataPath,
   "
 
   Data <- read.csv(DataPath,row.names = 1)
+  # or 
+  # Data <<- read_csv(DataPath) %>% column_to_rownames("X1") 
   Labels <- as.matrix(read.csv(LabelsPath))
   load(CV_RDataPath)
   Labels <- as.vector(Labels[,col_Index])
@@ -42,42 +46,49 @@ run_clustifyr <- function(DataPath,
   Pred_Labels_clustifyr <- list()
   Total_Time_clustifyr <- list()
   Data = t(as.matrix(Data))
+  
+  Normalized_data <- M3DropCleanData(Data,
+                                     labels = rownames(Data),
+                                     is.counts = TRUE,
+                                     suppress.plot = TRUE
+  )
+  fits <- M3DropDropoutModels(Normalized_data$data, suppress.plot = TRUE)
+  markers_M3Drop <<- M3DropFeatureSelection(Normalized_data$data,
+                                           mt_method = "fdr",
+                                           mt_threshold = 0.01,
+                                           suppress.plot = TRUE
+  )
 
   for (i in c(1:n_folds)){
     if(!is.null(GeneOrderPath) & !is.null (NumGenes)){
       start_time <- Sys.time()
-      ref = average_clusters(
+      ref <-  average_clusters(
         Data[as.vector(GenesOrder[c(1:NumGenes),i])+1,Train_Idx[[i]]],
-        Labels[Train_Idx[[i]]]
+        Labels[Train_Idx[[i]]],
+        if_log = FALSE
       )
       meta <- data.frame("cluster" = Labels[Test_Idx[[i]]])
       rownames(meta) <- Test_Idx[[i]]
+      
       res <- clustify(
         input = Data[as.vector(GenesOrder[c(1:NumGenes),i])+1,Test_Idx[[i]]],
-        ref = ref,
-        metadata = Labels[Test_Idx[[i]]]
+        ref_mat = ref,
+        metadata = Labels[Test_Idx[[i]]],
+        compute_method = compute_method,
+        if_log = F
       )
+      
       res2 <- cor_to_call(res, threshold = threshold, cluster_col = "cluster")
       meta2 <- call_to_metadata(res2, meta, cluster_col = "cluster")
       end_time <- Sys.time()
     }
     else{
       start_time <- Sys.time()
-      Normalized_data <- M3DropCleanData(Data[,Train_Idx[[i]]],
-                                         labels = rownames(Data[,Train_Idx[[i]]]),
-                                         is.counts = FALSE,
-                                         suppress.plot = TRUE
-      )
-      fits <- M3DropDropoutModels(Normalized_data$data, suppress.plot = TRUE)
-      markers_M3Drop <- M3DropFeatureSelection(Normalized_data$data,
-                                               mt_method = "fdr",
-                                               mt_threshold = 0.01,
-                                               suppress.plot = TRUE
-      )
 
-      ref = average_clusters(
+      ref <- average_clusters(
         Data[,Train_Idx[[i]]],
-        Labels[Train_Idx[[i]]]
+        Labels[Train_Idx[[i]]],
+        if_log = FALSE
       )
 
       meta <- data.frame("cluster" = Labels[Test_Idx[[i]]])
@@ -85,9 +96,11 @@ run_clustifyr <- function(DataPath,
 
       res <- clustify(
         input = Data[,Test_Idx[[i]]],
-        ref = ref,
+        ref_mat = ref,
         metadata = Labels[Test_Idx[[i]]],
-        query_genes = markers_M3Drop$Gene
+        query_genes = markers_M3Drop$Gene,
+        compute_method = compute_method,
+        if_log = F
       )
 
       res2 <- cor_to_call(
@@ -107,6 +120,7 @@ run_clustifyr <- function(DataPath,
     True_Labels_clustifyr[i] <- list(Labels[Test_Idx[[i]]])
     Pred_Labels_clustifyr[i] <- list(meta2$type)
   }
+  
   True_Labels_clustifyr <- as.vector(unlist(True_Labels_clustifyr))
   Pred_Labels_clustifyr <- as.vector(unlist(Pred_Labels_clustifyr))
   Total_Time_clustifyr <- as.vector(unlist(Total_Time_clustifyr))
