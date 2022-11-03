58ec64f6a6152c8891791e71de9422ff017db291
Jay Hesselberth <jay.hesselberth@gmail.com>
2022-11-03 17:14:26 -0600

    fix several broken links

--- data-raw/downrefs.R
+++ data-raw/downrefs.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 
 d1 <- c(
-    paste0("[", "ref_MCA", "](", "https://github.com/rnabioco/clustifyrdata/raw/master/data/ref_MCA.rda", ")"),
+    paste0("[", "ref_MCA", "](", "https://github.com/rnabioco/clustifyrdata/raw/main/data/ref_MCA.rda", ")"),
     "Mouse Cell Atlas",
     dim(clustifyrdata::ref_MCA)[2],
     dim(clustifyrdata::ref_MCA)[1],
@@ -10,7 +10,7 @@ d1 <- c(
 )
 
 d2 <- c(
-    paste0("[", "ref_tabula_muris_drop", "](", "https://github.com/rnabioco/clustifyrdata/raw/master/data/ref_tabula_muris_drop.rda", ")"),
+    paste0("[", "ref_tabula_muris_drop", "](", "https://github.com/rnabioco/clustifyrdata/raw/main/data/ref_tabula_muris_drop.rda", ")"),
     "Tabula Muris (10X)",
     dim(clustifyrdata::ref_tabula_muris_drop)[2],
     dim(clustifyrdata::ref_tabula_muris_drop)[1],
@@ -19,7 +19,7 @@ d2 <- c(
 )
 
 d3 <- c(
-    paste0("[", "ref_tabula_muris_facs", "](", "https://github.com/rnabioco/clustifyrdata/raw/master/data/ref_tabula_muris_facs.rda", ")"),
+    paste0("[", "ref_tabula_muris_facs", "](", "https://github.com/rnabioco/clustifyrdata/raw/main/data/ref_tabula_muris_facs.rda", ")"),
     "Tabula Muris (SmartSeq2)",
     dim(clustifyrdata::ref_tabula_muris_facs)[2],
     dim(clustifyrdata::ref_tabula_muris_facs)[1],
@@ -28,7 +28,7 @@ d3 <- c(
 )
 
 d4 <- c(
-    paste0("[", "ref_mouse.rnaseq", "](", "https://github.com/rnabioco/clustifyrdata/raw/master/data/ref_mouse.rnaseq.rda", ")"),
+    paste0("[", "ref_mouse.rnaseq", "](", "https://github.com/rnabioco/clustifyrdata/raw/main/data/ref_mouse.rnaseq.rda", ")"),
     "Mouse RNA-seq from 28 cell types",
     dim(clustifyrdata::ref_mouse.rnaseq)[2],
     dim(clustifyrdata::ref_mouse.rnaseq)[1],
@@ -37,7 +37,7 @@ d4 <- c(
 )
 
 d5 <- c(
-    paste0("[", "ref_moca_main", "](", "https://github.com/rnabioco/clustifyrdata/raw/master/data/ref_moca_main.rda", ")"),
+    paste0("[", "ref_moca_main", "](", "https://github.com/rnabioco/clustifyrdata/raw/main/data/ref_moca_main.rda", ")"),
     "Mouse Organogenesis Cell Atlas (main cell types)",
     dim(clustifyrdata::ref_moca_main)[2],
     dim(clustifyrdata::ref_moca_main)[1],
@@ -46,7 +46,7 @@ d5 <- c(
 )
 
 d6 <- c(
-    paste0("[", "ref_immgen", "](", "https://github.com/rnabioco/clustifyrdata/raw/master/data/ref_immgen.rda", ")"),
+    paste0("[", "ref_immgen", "](", "https://github.com/rnabioco/clustifyrdata/raw/main/data/ref_immgen.rda", ")"),
     "Mouse sorted immune cells",
     dim(clustifyrdata::ref_immgen)[2],
     dim(clustifyrdata::ref_immgen)[1],
@@ -55,7 +55,7 @@ d6 <- c(
 )
 
 d7 <- c(
-    paste0("[", "ref_hema_microarray", "](", "https://github.com/rnabioco/clustifyrdata/raw/master/data/ref_hema_microarray.rda", ")"),
+    paste0("[", "ref_hema_microarray", "](", "https://github.com/rnabioco/clustifyrdata/raw/main/data/ref_hema_microarray.rda", ")"),
     "Human hematopoietic cell microarray",
     dim(clustifyrdata::ref_hema_microarray)[2],
     dim(clustifyrdata::ref_hema_microarray)[1],
@@ -64,7 +64,7 @@ d7 <- c(
 )
 
 d8 <- c(
-    paste0("[", "ref_cortex_dev", "](", "https://github.com/rnabioco/clustifyrdata/raw/master/data/ref_cortex_dev.rda", ")"),
+    paste0("[", "ref_cortex_dev", "](", "https://github.com/rnabioco/clustifyrdata/raw/main/data/ref_cortex_dev.rda", ")"),
     "Human cortex development scRNA-seq",
     dim(clustifyrdata::ref_cortex_dev)[2],
     dim(clustifyrdata::ref_cortex_dev)[1],
@@ -73,7 +73,7 @@ d8 <- c(
 )
 
 d9 <- c(
-    paste0("[", "ref_pan_indrop", "](", "https://github.com/rnabioco/clustifyrdata/raw/master/data/ref_pan_indrop.rda", ")"),
+    paste0("[", "ref_pan_indrop", "](", "https://github.com/rnabioco/clustifyrdata/raw/main/data/ref_pan_indrop.rda", ")"),
     "Human pancreatic cell scRNA-seq (inDrop)",
     dim(clustifyrdata::ref_pan_indrop)[2],
     dim(clustifyrdata::ref_pan_indrop)[1],
@@ -82,7 +82,7 @@ d9 <- c(
 )
 
 d10 <- c(
-    paste0("[", "ref_pan_smartseq2", "](", "https://github.com/rnabioco/clustifyrdata/raw/master/data/ref_pan_smartseq2.rda", ")"),
+    paste0("[", "ref_pan_smartseq2", "](", "https://github.com/rnabioco/clustifyrdata/raw/main/data/ref_pan_smartseq2.rda", ")"),
     "Human pancreatic cell scRNA-seq (SmartSeq2)",
     dim(clustifyrdata::ref_pan_smartseq2)[2],
     dim(clustifyrdata::ref_pan_smartseq2)[1],
@@ -92,5 +92,5 @@ d10 <- c(
 
 d <- data.frame(d1, d2, d3, d4, d5, d6, d7, d8, d9, d10) %>% t()
 colnames(d) <- c("name", "desc", "ntypes", "ngenes", "org", "from_pub")
-downrefs <- d %>% as.tibble()
+downrefs <- as_tibble(d) 
 usethis::use_data(downrefs, compress = "xz", overwrite = TRUE)
